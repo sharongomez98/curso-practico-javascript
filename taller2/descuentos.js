e3d187339ab3a8ca2f3edf6410ffa39b8a247d73
Sharon Gómez <sharonrubi98@gmail.com>
2022-02-02 01:31:47 -0600

    base html for taller 2

--- taller2/descuentos.js
+++ taller2/descuentos.js
@@ -1,17 +1,17 @@
-const precioOriginal = 120;
-const descuento = 18;
-
 const calcularPrecioConDescuento = (precio, descuento) => {
-    const porcentajePrecioConDescuento = 100 - descuento;
-    const precioConDescuento = (precio * porcentajePrecioConDescuento)/100;
-    return precioConDescuento;
-}
+  const porcentajePrecioConDescuento = 100 - descuento;
+  const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+  return precioConDescuento;
+};
 
+const onClickButtonPriceDiscount = () => {
+  const priceInput = document.getElementById("input-price");
+  const price = priceInput.value;
 
+  const discountInput = document.getElementById("input-discount");
+  const discount = discountInput.value;
 
-console.log({
-    precioOriginal,
-    descuento
-});
-
-console.log(calcularPrecioConDescuento(precioOriginal, descuento));
+  priceWithDiscount = calcularPrecioConDescuento(price, discount);
+  document.getElementById("PriceResult").innerText =
+    "El precio final a pagar es de: $ " + priceWithDiscount;
+};
