b4b4ced57b8dd2f2f6818cd5b7b440df0c32a089
Sharon Gómez <sharonrubi98@gmail.com>
2022-02-02 22:43:39 -0600

    challenge add coupon discount taller 2

--- taller2/descuentos.js
+++ taller2/descuentos.js
@@ -1,17 +1,49 @@
-const calcularPrecioConDescuento = (precio, descuento) => {
-  const porcentajePrecioConDescuento = 100 - descuento;
+const availableCoupons = [
+  { code: "ABC123", discount: 10 },
+  { code: "DESC15", discount: 15 },
+  { code: "OFERTA20", discount: 20 },
+];
+
+const calcularPrecioConDescuento = (precio, descuento, coupon) => {
+  const porcentajeDescuento = descuento + coupon;
+  const porcentajePrecioConDescuento = 100 - porcentajeDescuento;
+  console.log({
+    precio,
+    descuento,
+    coupon,
+    porcentajeDescuento,
+    porcentajePrecioConDescuento,
+  });
   const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
-  return precioConDescuento;
+  const totalDescuento = precio - precioConDescuento;
+  
+  return { precioConDescuento, porcentajeDescuento, totalDescuento };
 };
 
 const onClickButtonPriceDiscount = () => {
   const priceInput = document.getElementById("input-price");
-  const price = priceInput.value;
+  const price = parseFloat(priceInput.value) || 0;
 
   const discountInput = document.getElementById("input-discount");
-  const discount = discountInput.value;
+  const discount = parseFloat(discountInput.value) || 0;
 
-  priceWithDiscount = calcularPrecioConDescuento(price, discount);
-  document.getElementById("PriceResult").innerText =
-    "El precio final a pagar es de: $ " + priceWithDiscount;
+  const couponInput = document.getElementById("input-coupon");
+  const coupon = couponInput.value;
+
+  let couponDiscount = availableCoupons
+    .filter((x) => x.code == coupon)
+    .map((ele) => ele.discount);
+  couponDiscount = couponDiscount.shift();
+  couponDiscount = couponDiscount ? couponDiscount : 0;
+  console.log(couponDiscount);
+
+  priceValues = calcularPrecioConDescuento(price, discount, couponDiscount);
+  document.getElementById("price-result").innerText =
+    "El precio final a pagar es de: $ " + priceValues.precioConDescuento;
+  document.getElementById("porcentage-save").innerText =
+    "El porcentaje total aplicado es de: " +
+    priceValues.porcentajeDescuento +
+    "%";
+    document.getElementById("price-save").innerText =
+    "Usted ahorro: $ " + priceValues.totalDescuento;
 };
